5d46e5ced28315b5e076113ea9f036d2dc539024
sinper0079 <sinper0079@yahoo.com.hk>
2021-03-28 16:16:54 +0800

    minor fix compile error

--- src/net/SPServer/src/SPNetServer.cpp
+++ src/net/SPServer/src/SPNetServer.cpp
@@ -14,10 +14,30 @@ void SPNetServer::_update()
 
 
 }
+void SPNetServer::StartListen(const SockAddr& addr)
+{
+	SITA_LOG("Start Listening");
+	_listenSock.createTCP();
+	_listenSock.setNonBlocking(true);
+	//_listenSock.setReuseAddr(true);
+	//addr.
+	//addr._addr.sa_data setIPv4(0, 0, 0, 0);
+	//addr.setPort(3300);
+	_listenSock.bind(hostname, static_cast<uint16_t>(port));
+	_listenSock.listen();
+	_listenSock.accept(_sock);
+
+
+}
+void SPNetServer::StopListen()
+{
+
+
+}
 void SPNetServer::onDeinitServer()
 {
 	SITA_LOG("onDeinitServer");
-	stopListen();
+	StopListen();
 	_listenSock.close();
 	_sock.close();
 	isServerRunning = false;
@@ -26,5 +46,5 @@ void SPNetServer::onDeinitServer()
 void SPNetServer::onInitServer()
 {
 	SITA_LOG("onInitServer");
-	listen(sockAddr);
+	StartListen(sockAddr);
 }
--- src/net/SPServer/src/SPNetServer.h
+++ src/net/SPServer/src/SPNetServer.h
@@ -8,9 +8,17 @@ class SPNetServer : NetEngine {
 	virtual void onInitServer();
 	virtual void onDeinitServer();
 	void _update();
-
+	void StartListen(const SockAddr& addr);
+	void StopListen();
 	private:
 	bool isServerRunning = true;
 
+	private:
 
+	Socket _listenSock;
+	Socket _sock;
+	String _sockBuf;
+	int port = 30000;
+	SockAddr sockAddr;
+	char hostname[256] = "localhost";
 };
